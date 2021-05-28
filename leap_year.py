5d38acc5d01b734d22f417c95c9c27b447c0bae9
SS-CC-YY <songchen@oregonstate.edu>
2021-05-28 08:50:47 +0800

    rewrite the leap_year code in hw1, and the first test is test the year 2020 to make true the new code could run correctly

---
+++ leap_year.py
@@ -0,0 +1,12 @@
+
+def leap_year(year):
+    if(year % 4 == 0 and year % 100 != 0 or year % 400 == 0):
+        return "It is a leap year!"
+    else:
+        return "It is not a leap year!"
+
+def main():
+    print(leap_year(2020))
+
+if __name__ == "__main__":
+    main()
